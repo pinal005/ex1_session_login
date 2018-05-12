3bfdaa6f37d96617a75a7331e952f71fb12f89ee
Padaliya <Padaliya@192.168.1.5>
2018-05-12 12:55:33 +0530

    error msg changed

--- httpsessionlogin/src/LoginServlet.java
+++ httpsessionlogin/src/LoginServlet.java
@@ -23,7 +23,7 @@ public class LoginServlet extends HttpServlet {
 		session.setAttribute("name",name);
 		}
 		else{
-			out.print("Sorry, username or password error!");
+			out.print("Sorry, username or password error!!");
 			request.getRequestDispatcher("login.html").include(request, response);
 		}
 		out.close();
